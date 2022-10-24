2dc65f8261f3b93ed29ab013bfc06b175dec1763
liuji <1546243245@qq.com>
2022-10-24 23:13:02 +0800

    pass wrapper int

--- libsponge/wrapping_integers.cc
+++ libsponge/wrapping_integers.cc
@@ -1,12 +1,16 @@
 #include "wrapping_integers.hh"
 
+#include <algorithm>
+#include <cstdint>
+#include <vector>
+
 // Dummy implementation of a 32-bit wrapping integer
 
 // For Lab 2, please replace with a real implementation that passes the
 // automated checks run by `make check_lab2`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
@@ -14,8 +18,9 @@ using namespace std;
 //! \param n The input absolute 64-bit sequence number
 //! \param isn The initial sequence number
 WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
-    DUMMY_CODE(n, isn);
-    return WrappingInt32{0};
+    uint64_t diff = n + isn.raw_value();
+    uint64_t mask = (1ULL << 32) - 1;
+    return WrappingInt32{static_cast<uint32_t>(diff & mask)};
 }
 
 //! Transform a WrappingInt32 into an "absolute" 64-bit sequence number (zero-indexed)
@@ -29,6 +34,15 @@ WrappingInt32 wrap(uint64_t n, WrappingInt32 isn) {
 //! and the other stream runs from the remote TCPSender to the local TCPReceiver and
 //! has a different ISN.
 uint64_t unwrap(WrappingInt32 n, WrappingInt32 isn, uint64_t checkpoint) {
-    DUMMY_CODE(n, isn, checkpoint);
-    return {};
+    uint32_t diff = n.raw_value() - isn.raw_value();
+    constexpr uint64_t mask = ((1ULL << 32) - 1) << 32;
+    std::vector<uint64_t> nums;
+    nums.emplace_back((checkpoint & mask) | diff);
+    nums.emplace_back(((checkpoint + (1ULL << 32)) & mask) | diff);
+    nums.emplace_back(((checkpoint - (1ULL << 32)) & mask) | diff);
+    return *std::min_element(nums.begin(), nums.end(), [&](uint64_t a, uint64_t b) {
+        uint64_t dis_a = checkpoint > a ? checkpoint - a : a - checkpoint;
+        uint64_t dis_b = checkpoint > b ? checkpoint - b : b - checkpoint;
+        return dis_a < dis_b;
+    });
 }
